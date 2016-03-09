9a78137f5e5f169f4b7d5026589901494dba1db0
GuillaumeRot <guillaume.rot@gmail.com>
2016-03-09 19:54:08 +0100

    First commit

---
+++ app/src/main/java/com/euro16/FacebookConnexion.java
@@ -0,0 +1,13 @@
+package com.euro16;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FacebookConnexion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facebook_connexion);
+    }
+}
