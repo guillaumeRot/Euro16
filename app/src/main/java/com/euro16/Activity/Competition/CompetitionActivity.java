13b9ff50fbb25a3575fa35df6900b26f30848e40
guillaumeRot <guillaume.rot@gmail.com>
2016-05-29 20:53:09 +0200

    En cours : Page de connexion Facebook A faire : Changer police menu + icones

--- app/src/main/java/com/euro16/Activity/Competition/CompetitionActivity.java
+++ app/src/main/java/com/euro16/Activity/Competition/CompetitionActivity.java
@@ -5,6 +5,7 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +13,8 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Spannable;
+import android.text.SpannableString;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -73,6 +76,8 @@ public class CompetitionActivity extends AppCompatActivity implements Navigation
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Typeface face = Typeface.createFromAsset(getAssets(), "fonts/font_euro.ttf");
 
         pronoMenuActivated = false;
 
@@ -145,8 +150,8 @@ public class CompetitionActivity extends AppCompatActivity implements Navigation
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.right_menu, menu);
+
         return true;
     }
 
